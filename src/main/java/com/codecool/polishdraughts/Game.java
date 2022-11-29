33547977aba0be5f9fba96e66d4b539e14de9968
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2022-11-29 19:13:26 +0100

    check commit before pull

--- src/main/java/com/codecool/polishdraughts/Game.java
+++ src/main/java/com/codecool/polishdraughts/Game.java
@@ -7,5 +7,14 @@ public class Game {
     public static void main(String[] args) {
         Board board = new Board(11);
         System.out.println(board);
+
+    }
+
+    public boolean validateStartingPos(int[][] board, int x, int y) {
+        return true;
+    }
+
+    public boolean validateEndingPos(int[][] board, int x, int y) {
+        return true;
     }
 }
