839e036353498ca34369d159a36ec539c5138c69
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2022-11-30 13:21:01 +0100

    Get the launch movement function.

--- src/main/java/com/codecool/polishdraughts/Board.java
+++ src/main/java/com/codecool/polishdraughts/Board.java
@@ -1,11 +1,11 @@
 package com.codecool.polishdraughts;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Board {
     private int n;
-    private Pawn[][] fields;
+    private static Pawn[][] fields;
 
     /* Option 1 */
     public Pawn getField(int x, int y) {
@@ -71,6 +71,7 @@ public class Board {
         return boardAsString;
     }
 
+
     public void removePawn(int x, int y){
         this.fields[x][y] = null;
     }
@@ -78,5 +79,39 @@ public class Board {
     /* for test only */
     public void setPawn(Pawn pawn) {
         fields[pawn.getCoordinates().getXCoordinate()][pawn.getCoordinates().getXCoordinate()] = pawn;
+    }
+
+    public static int[] getStartingMove() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the starting position:");
+        String position = scanner.nextLine().toUpperCase();
+
+        while (!isValid(position, fields)) {
+            System.out.print("Wrong coordinates! Enter correct position: ");
+            position = scanner.nextLine().toUpperCase();
+        }
+        String[] coordinates = position.split("", 2);
+        int firstCoordinate = position.charAt(0) - 65;
+        System.out.println(firstCoordinate);
+        int secondCoordinate = Integer.parseInt(coordinates[1]) - 1;
+        System.out.println(secondCoordinate);
+
+        return new int[]{firstCoordinate, secondCoordinate};
+    }
+    private static boolean isValid(String str, Pawn[][] board) {
+        if (str.length() != 2 && str.length() != 3) return false;
+        char col = str.charAt(0);
+        if (col < 'A' || col >= 'A' + board[0].length) {
+            return false;
+        }
+        try {
+            int row = Integer.parseInt(str.substring(1));
+            if (row < 1 || row > board.length) {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
