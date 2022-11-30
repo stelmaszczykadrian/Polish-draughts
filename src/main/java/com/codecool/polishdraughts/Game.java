6c4e22fc265d25a1731d3fd32d156469d7a28106
karolina <karolinalodzinska98@gmail.com>
2022-11-30 21:10:57 +0100

    Implementation of playRound method

--- src/main/java/com/codecool/polishdraughts/Game.java
+++ src/main/java/com/codecool/polishdraughts/Game.java
@@ -14,12 +14,30 @@ public class Game {
 //        System.out.println(board.getField(1,1).validateMove(board, 3,3));
 //        }
 
+    public static void playRound(Board board, char currentPlayer){
+        displayTurn(currentPlayer);
+        Board.getStartingMove();
+//        Board.getEndingMove();
+//        metoda tryToMakeMove
+//        metoda movePawn
+        System.out.println(board);
+    }
 
-    public static void Start() {
+    private static void displayTurn(char currentPlayer) {
+        if (currentPlayer == 'w'){
+            System.out.println("White turn!");
+        }
+        else {
+            System.out.println("Black turn!");
+        }
+    }
+
+    public static void start() {
         Display display = new Display();
         Scanner scan = new Scanner(System.in);
+        Board board = new Board(10);
+        display.printMessage("Welcome in Polish Draughts Game! White starts!");
         boolean wrongUserInput = true;
-        display.printMessage("Welcome in Polish Draughts Game!");
         while (wrongUserInput) {
             display.printSameLineMsg("Put the size of the board. It need to be from 10 to 20: ");
             String userInput = scan.nextLine();
@@ -27,11 +45,25 @@ public class Game {
             int intUserInput = Integer.parseInt(userInput);
             if (intUserInput >= 10 && intUserInput <= 20) {
                 wrongUserInput = false;
-                Board board = new Board(intUserInput);
+                board = new Board(intUserInput);
                 System.out.println(board);
             } else {
                 display.printMessage("Only numbers from 10 to 20 are available.Try again.");
             }
         }
+
+        char currentPlayer = 'w';
+
+//        wersja dla testu
+        while (true) {
+            playRound(board, currentPlayer);
+            currentPlayer = currentPlayer == 'w' ? 'b' : 'w';
+        }
+
+//        wersja końcowa, jak będziemy mieli metodę checkForWinner()
+//        while (checkForWinner() == false) {
+//            playRound(board, currentPlayer);
+//            currentPlayer = currentPlayer == 'w' ? 'b' : 'w';
+//        }
     }
 }
